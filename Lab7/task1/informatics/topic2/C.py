3bd0ca9f83e1768f59cf77ebd0f57fbdfe65d5b3
Fomichyova Ekaterina <katjushaef@gmail.com>
2026-03-14 12:13:07 +0600

    Create C.py

---
+++ Lab7/task1/informatics/topic2/C.py
@@ -0,0 +1,12 @@
+correct = int(input())
+student = int(input())
+if correct == 1:
+    if student == 1:
+        print("YES")
+    else:
+        print("NO")
+else:
+    if student != 1:
+        print("YES")
+    else:
+        print("NO")
