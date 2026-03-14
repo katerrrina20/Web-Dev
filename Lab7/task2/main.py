e9dfbad1984bbd5565bc85b9519337881eff0e08
Fomichyova Ekaterina <katjushaef@gmail.com>
2026-03-15 03:01:53 +0600

    Add files via upload

---
+++ Lab7/task2/main.py
@@ -0,0 +1,20 @@
+from models import Animal, Dog, Cat
+
+
+def main():
+
+    animal = Animal("GenericAnimal", 5, "gray")
+    dog = Dog("Buddy", 3, "brown", "Labrador")
+    cat = Cat("Whiskers", 2, "white", 9)
+
+    animals = [animal, dog, cat]
+
+    for a in animals:
+        print(a)
+        print(a.info())
+        print(a.speak())
+        print()
+
+
+if __name__ == "__main__":
+    main()
