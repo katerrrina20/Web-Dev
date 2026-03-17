d10734012fe29e473b6e29d4525221f03b0c27b9
Fomichyova Ekaterina <katjushaef@gmail.com>
2026-03-17 14:16:17 +0600

    Update main.py

--- Lab7/task2/main.py
+++ Lab7/task2/main.py
@@ -1,20 +1,13 @@
 from models import Animal, Dog, Cat
-
-
 def main():
-
     animal = Animal("GenericAnimal", 5, "gray")
     dog = Dog("Max", 3, "brown", "Labrador")
     cat = Cat("Barsik", 2, "white", 9)
-
     animals = [animal, dog, cat]
-
     for a in animals:
         print(a)
         print(a.info())
         print(a.speak())
         print()
-
-
 if __name__ == "__main__":
     main()
