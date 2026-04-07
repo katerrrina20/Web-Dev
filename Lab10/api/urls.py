3f8ec1b37c392ed06f958437c20a67d1b85a30f7
Fomichyova Ekaterina <katjushaef@gmail.com>
2026-04-07 21:20:46 +0600

    Add files via upload

---
+++ Lab10/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from api import views  
+urlpatterns = [
+    path('categories/', views.CategoryListAPIView.as_view()),
+    path('categories/<int:category_id>/', views.CategoryDetailAPIView.as_view()),
+    path('categories/<int:category_id>/products/', views.CategoryProductsAPIView.as_view()),
+    path('products/', views.ProductListAPIView.as_view()),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view()),
+]
