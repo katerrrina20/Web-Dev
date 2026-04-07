3298593aef83972705cdaf148f4a896f964bf08c
Fomichyova Ekaterina <katjushaef@gmail.com>
2026-04-07 21:19:09 +0600

    Add files via upload

---
+++ Lab10/api/views/mixins.py
@@ -0,0 +1,40 @@
+from rest_framework import generics
+from rest_framework import mixins
+from api.models import Product, Category
+from api.serializers import ProductSerializer, CategorySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class CategoryListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    def get(self, request, *args, **kwargs): return self.list(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs): return self.create(request, *args, **kwargs)
+
+class CategoryDetailAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_url_kwarg = 'category_id'
+    def get(self, request, *args, **kwargs): return self.retrieve(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs): return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs): return self.destroy(request, *args, **kwargs)
+
+class ProductListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    def get(self, request, *args, **kwargs): return self.list(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs): return self.create(request, *args, **kwargs)
+
+class ProductDetailAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
+    def get(self, request, *args, **kwargs): return self.retrieve(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs): return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs): return self.destroy(request, *args, **kwargs)
+
+class CategoryProductsAPIView(APIView):
+    def get(self, request, category_id):
+        products = Product.objects.filter(category_id=category_id)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
